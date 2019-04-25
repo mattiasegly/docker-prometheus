cb316098060e20e811c057d922f8d78a5f47f4be
mattiasegly <48134992+mattiasegly@users.noreply.github.com>
2019-04-25 17:04:22 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+    
+#!/bin/bash
+
+echo "Create Prometheus data folder if it doesn't exist..."
+if [ ! -d /prometheus/data ]; then
+mkdir /prometheus/data
+fi
+
+echo "Set folder access..."
+chown nobody:nogroup /prometheus/data
+
+echo "Finished! Run container."
+exec "$@"
