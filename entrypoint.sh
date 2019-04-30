31ea4f527a7113f31778e05ce67f4b2446bb4d98
mattiasegly <48134992+mattiasegly@users.noreply.github.com>
2019-04-30 12:23:59 +0200

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Create Prometheus data folder if it doesn't exist..."
-if [ ! -d /prometheus/data ]; then
-mkdir /prometheus/data
-fi
-
-echo "Finished! Run container."
-exec "$@"
